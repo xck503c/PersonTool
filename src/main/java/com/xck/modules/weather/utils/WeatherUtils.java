c005b6b8d9cbc55e5931e0d5e60c37bd6f12d73b
xck503c <2198494838@qq.com>
2017-01-18 01:30:10 +0800

    add three check json node method

--- src/main/java/com/xck/modules/weather/utils/WeatherUtils.java
+++ src/main/java/com/xck/modules/weather/utils/WeatherUtils.java
@@ -7,30 +7,24 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 
-import org.apache.commons.collections.CollectionUtils;
-
-import com.xck.modules.weather.entity.Weather;
-
-import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 public class WeatherUtils {
-	public final static String WEATHER_URL = "https://free-api.heweather.com/v5/now";
+	public final static String WEATHER_URL = "https://free-api.heweather.com/v5/";
 	public final static String KEY = "1b32a177da3a45b9b5d2841058fa1e4e";
 	
-	public static String request(String city){
+	public static String request(String city, String type){
 		BufferedReader reader = null;
 		String result = null;
 		StringBuffer sb = new StringBuffer();
-		String httpUrl = WEATHER_URL + "?city=" + city + "&key=" + KEY;
+		String httpUrl = WEATHER_URL + type + "?city=" + city + "&key=" + KEY;
+		System.out.println(httpUrl);
 		
 		try {
 			URL url = new URL(httpUrl);
 			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 			conn.setRequestMethod("GET");
-			System.out.println(conn.getURL());
 			conn.connect();
 			InputStream is = conn.getInputStream();
 			reader = new BufferedReader(new InputStreamReader(is));
@@ -42,53 +36,31 @@ public class WeatherUtils {
 			reader.close();
 			result = sb.toString();
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return result;
 	}
 	
-	public static ArrayList<Weather> pareseJsonData(String jsonData){
-		ArrayList<Weather> cityList = new ArrayList();
-		
-		JSONObject json = JSONObject.fromObject(jsonData);
-		JSONArray results = json.getJSONArray("HeWeather5");
-		
-		if(CollectionUtils.isNotEmpty(results)){
-			for (int i=0; i<results.size(); i++){
-				JSONObject jsonObj = results.getJSONObject(i);
-				System.out.println(jsonObj.toString());
-				Weather w = new Weather();
-				JSONObject basicObj = jsonObj.getJSONObject("basic");
-				
-				w.setId(basicObj.getString("id"));
-				w.setCity(basicObj.getString("city"));
-				w.setCnty(basicObj.getString("cnty"));
-				JSONObject basicUTObj = basicObj.getJSONObject("update");
-				w.setUpdate(basicUTObj.getString("loc"));
-				
-				JSONObject nowObj = jsonObj.getJSONObject("now");
-				JSONObject condObj = nowObj.getJSONObject("cond");
-				w.setCond(condObj.getString("txt"));
-				w.setHum(nowObj.getInt("hum"));
-				w.setPcpn(nowObj.getDouble("pcpn"));
-				//w.setPop(nowObj.getInt("pop"));
-				w.setPres(nowObj.getInt("pres"));
-				w.setTmp(nowObj.getInt("tmp"));
-				w.setVis(nowObj.getInt("vis"));
-				
-				JSONObject windObj = nowObj.getJSONObject("wind");
-				w.setDeg(windObj.getInt("deg"));
-				w.setDir(windObj.getString("dir"));
-				w.setSc(windObj.getString("sc"));
-				w.setSpd(windObj.getInt("spd"));
-				
-				cityList.add(w);
-			}
+	public static String checkJSONNodeString(JSONObject obj, String key){
+		if(!obj.isNullObject() && obj.has(key)){
+			return obj.getString(key);
 		}
-		return cityList;
+		return "";
+	}
+	
+	public static Integer checkJSONNodeInteger(JSONObject obj, String key){
+		if(!obj.isNullObject() && obj.has(key)){
+			return obj.getInt(key);
+		}
+		return 0;
+	}
+	
+	public static Double checkJSONNodeDouble(JSONObject obj, String key){
+		if(!obj.isNullObject() && obj.has(key)){
+			return obj.getDouble(key);
+		}
+		return 0.0;
 	}
 }
