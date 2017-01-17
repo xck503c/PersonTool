f4cbc0675e68b5180a2a3c13b4c68075389164f8
xck503c <2198494838@qq.com>
2017-01-18 01:21:20 +0800

    add abstract class

---
+++ src/main/java/com/xck/modules/weather/service/BaseCityService.java
@@ -0,0 +1,12 @@
+package com.xck.modules.weather.service;
+
+/*
+ * 天气 service基类
+ * @author xck
+ * @version 2017-1-16
+ * */
+
+public abstract class BaseCityService {
+	
+	public abstract String get(String city);
+}
