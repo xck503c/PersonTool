f10c5475923fcd780d41b8130f5acfce581d750c
xck503c <2198494838@qq.com>
2017-01-18 22:54:19 +0800

    add get basic city list by key in prov and get all info in prov controller

--- src/main/java/com/xck/modules/weather/web/BasicCityController.java
+++ src/main/java/com/xck/modules/weather/web/BasicCityController.java
@@ -1,8 +1,8 @@
 package com.xck.modules.weather.web;
 
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,33 +10,45 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.xck.modules.weather.entity.BasicCity;
 import com.xck.modules.weather.service.BasicCityService;
 
 /*
- * @version 2017-1-17
+ * 城市基础信息Controller
+ * @author xck503c
+ * @version 2017-1-18
  * */
 
 @Controller
-@RequestMapping(value="/f/weather")
+@RequestMapping(value="/f/weather/basicCity")
 public class BasicCityController extends BaseCityController{
 	
 	@Autowired
 	private BasicCityService basicCityService;
 	
 	@RequestMapping(value="/index", method={RequestMethod.POST, RequestMethod.GET})
-	public String index(HttpServletRequest request, Model model) {
+	public String index(HttpServletRequest request, String provC, String provE, Model model) {
+		HttpSession session = request.getSession(true);
+		basicCityService.index(provC, provE, session, model);
 		return "weather/weather_index";
 	}
 	
 	@RequestMapping(value="/query", method={RequestMethod.POST, RequestMethod.GET})
-	public String query(HttpServletRequest request, String city, Model model) {
-		String jsonData = basicCityService.get(city);
-		System.out.println(jsonData);
-		List<BasicCity> list = basicCityService.getData(jsonData);
-		
-		model.addAttribute("jsonData", jsonData);
-		model.addAttribute("cityList", list);
-		return "weather/weather_city";
+	public String query(HttpServletRequest request, String city, String provC, String provE, Model model) {
+		HttpSession session = request.getSession(true);
+		boolean result = basicCityService.query(city, provC, provE, session, model);
+		if(result){
+			return "weather/weather_city";
+		}
+		return "weather/weather_index";
+	}
+	
+	@RequestMapping(value="/prov", method={RequestMethod.POST, RequestMethod.GET})
+	public String prov(HttpServletRequest request, String provC, String provE, Model model) {
+		HttpSession session = request.getSession(true);
+		boolean result = basicCityService.prov(provC, provE, session, model);
+		if(result){
+			return "weather/weather_city";
+		}
+		return "weather/weather_index";
 	}
 }
